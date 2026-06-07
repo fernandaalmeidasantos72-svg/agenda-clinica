9b8151bf53affa359631a002d367721ccb52aa22
fernandaalmeidasantos72-svg <fernandaalmeidasantos72@gmail.com>
2026-06-07 19:03:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
-const CACHE = "agenda-clinica-v1";
-const FILES = ["/", "/index.html"];
+const CACHE = "agenda-clinica-v2";
+const FILES = [
+  "/agenda-clinica/",
+  "/agenda-clinica/index.html",
+  "/agenda-clinica/manifest.json",
+  "/agenda-clinica/icon-192.png",
+  "/agenda-clinica/icon-512.png"
+];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("/index.html"))));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("/agenda-clinica/index.html")))
+  );
 });
